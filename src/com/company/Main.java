10f2ba56a7ae8c2f68b07626bfb11ab961f84941
Zeyu Zhao <zeyu.zhao@sjsu.edu>
2020-09-08 14:22:46 -0700

    commit summary

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(" /\\_/\\");
+        System.out.println("( ' ' )");
+        System.out.println("(  -  )");
+        System.out.println(" | | |");
+        System.out.println("(__|__)");
+    }
+}
